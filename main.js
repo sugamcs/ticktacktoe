9ffdc628a98612292fa3dfa63a2934e595b7c8b4
sugam <sugambhargav@gmail.com>
2023-10-28 15:11:35 +0530

    Transfer findTurn and switchTurn functions into a module method and create placeMarker for controlFlow of the game

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 function gameBoard() {
-        return {cells: [null, null, null, null, null, null, null, null, null]
-        }
-    }
+  return {cells: [null, null, null, null, null, null, null, null, null]
+  }
+}
 const board1 = gameBoard();
 board1.cells[2] = "X"
 // console.log(board1)
 
 
 function boardRender(){
-    board1.cells.forEach((cell) => {
-        const cellElement = document.createElement('div')
-        cellElement.textContent = cell
-        document.body.append(cellElement)
+board1.cells.forEach((cell) => {
+  const cellElement = document.createElement('div')
+  cellElement.textContent = cell
+  document.body.append(cellElement)
 })
 }
 
@@ -20,36 +20,50 @@ boardRender()
 
 
 function createPlayers(name, marker, turn){
-    const playerCreated = {name:name, marker:marker, turn:turn}
-    // document.body.append(playerCreated)
+const playerCreated = {name:name, marker:marker, turn:turn}
+// document.body.append(playerCreated)
 
-    return playerCreated
+return playerCreated
 
 }
 const player1 = createPlayers("Player 1", "X", false)
 const player2 = createPlayers("Player 2", "0", true)
 
+let controlFlow = {
+  findTurn: function(){
+      let currentPlayer;
+      if (player1.turn == true) {
+      currentPlayer = player1
+      }else{ 
+      currentPlayer = player2
+      }
+      return currentPlayer
+      },
+      switchTurn: function() {
+      player1.turn = !player1.turn
+      player2.turn = !player2.turn
+      },
 
-function findTurn(){
-  let currentPlayer;
-  if (player1.turn == true) {
-    currentPlayer = player1
-  }else{ 
-    currentPlayer = player2
-  }
-  return currentPlayer
+  placeMarker: function(event){
+      if (cell.textContent == "" && player1.turn == true){ //I can also do only(player1.turn)
+          if(event.target == cell){
+              let markerPlaced = event.target.textContent   
+              markerPlaced = player1.marker
+          
+          }else if(cell.textContent == "" && player2.turn == true) {
+              if(event.target == cell){
+              let markerPlaced = event.target.textContent   
+              markerPlaced = player2.marker
+          }
+      }
+      }
+      return markerPlaced
+
+
+
+}
 }
 
-function switchTurn() {
-   player1.turn = !player1.turn
-   player2.turn = !player2.turn
-}
-
-console.log(player1.turn)
-console.log(player2.turn)
-switchTurn()
-console.log(player1.turn)
-console.log(player2.turn)
-switchTurn()
-console.log(player1.turn)
-console.log(player2.turn)
+const p1 = controlFlow.findTurn()
+console.log(p1)
+placeMarker()
--- pra.js
+++
@@ -1,1 +0,0 @@
-// misc code file 
