d4d383e2a26dc8cb159854947b31967bbb718c8b
sugam <sugambhargav@gmail.com>
2023-10-29 00:47:18 +0530

    Give each cell their unique id and add them to a class, also fix minor code errors and delete unnecessary files

--- main.js
+++ main.js
@@ -1,76 +1,53 @@
 function gameBoard() {
-  return {cells: [null, null, null, null, null, null, null, null, null]
-  }
+  return { cells: [null, null, "X", null, null, null, null, null, null] };
 }
+
 const board1 = gameBoard();
-board1.cells[2] = "X"
-// console.log(board1)
 
+const container = document.querySelector('.container');
 
-function boardRender(){
-board1.cells.forEach((cell) => {
-  const cellElement = document.createElement('div')
-  cellElement.textContent = cell
-  cellElement.id = `cells`
-  document.body.append(cellElement)
-})
+function boardRender() {
+  board1.cells.forEach((cell, index) => {
+      const cellElement = document.createElement('div');
+      cellElement.textContent = cell
+      cellElement.classList.add("cells");
+      cellElement.id = `cell-${index}`;
+      container.appendChild(cellElement);
+
+      
+  });
 }
 
+boardRender();
+const cellElements = document.querySelectorAll('.cells');
 
-boardRender()
 
-
-function createPlayers(name, marker, turn){
-const playerCreated = {name:name, marker:marker, turn:turn}
-// document.body.append(playerCreated)
-
-return playerCreated
-
+function createPlayers(name, marker, turn) {
+  return { name: name, marker: marker, turn: turn };
 }
-const player1 = createPlayers("Player 1", "X", false)
-const player2 = createPlayers("Player 2", "0", true)
+
+const player1 = createPlayers("Player 1", "X", true);
+const player2 = createPlayers("Player 2", "O", false);
 
 let controlFlow = {
-  findTurn: function(){
-      let currentPlayer;
-      if (player1.turn == true) {
-      currentPlayer = player1
-      }else{ 
-      currentPlayer = player2
+  findTurn: function () {
+      return player1.turn ? player1 : player2;
+  },
+  switchTurn: function () {
+      player1.turn = !player1.turn;
+      player2.turn = !player2.turn;
+  },
+  placeMarker: function (event) {
+      let currentPlayer = controlFlow.findTurn();
+      if (event.target.textContent === '' && currentPlayer.turn) {
+          event.target.textContent = currentPlayer.marker;
+          controlFlow.switchTurn();
       }
-      return currentPlayer
-      },
-
-
-      switchTurn: function() {
-      player1.turn = !player1.turn
-      player2.turn = !player2.turn
-      },
-
-
-      placeMarker: function (event) {
-        const currentPlayer = controlFlow.findTurn();
-        if (event.target.textContent === '' && currentPlayer.turn) {
-            event.target.textContent = currentPlayer.marker;
-            controlFlow.switchTurn();
-        }
-    },
-      // winnerStatus: function {
-      //   if()
-      // }
+  },
 };
 
-// const currentPlayer = controlFlow.findTurn();
-// console.log(currentPlayer);
-// const cellElements = document.querySelectorAll('div');
-// const container = document.querySelector('.container')
-// cellElements.forEach((cell) => {
-//   container.append(cellElements)
-//     cell.addEventListener('click', controlFlow.placeMarker);
-// });
-const cellElements = document.querySelectorAll('#cells'); // Select the cells by their ID
-const container = document.querySelector('.container');
-cellElements.forEach((cell) => {
-    container.append(cell); // Append each cell individually
-    cell.addEventListener('click', controlFlow.placeMarker);
+const checkerF = cellElements.forEach((cell) => {
+  console.log("I am a cell")
+  cell.addEventListener('click', controlFlow.placeMarker);
 });
+console.log(checkerF)
