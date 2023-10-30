3095e49265f348ab7ea67115acf889d6b7282658
sugam <sugambhargav@gmail.com>
2023-10-31 01:34:51 +0530

    Create declareWinner and declareDraw function, add html form for asking player names for next step

--- main.js
+++ main.js
@@ -14,16 +14,14 @@ function boardRender() {
         container.appendChild(cellElement);
     });
 }
-
 boardRender();
 const cellElements = document.querySelectorAll('.cells');
 
 function createPlayers(name, marker, turn) {
     return { name: name, marker: marker, turn: turn };
 }
-
-const player1 = createPlayers("Player 1", "X", true);
-const player2 = createPlayers("Player 2", "O", false);
+const player1 = createPlayers("Player1", "X", true);
+const player2 = createPlayers("Player2", "O", false);
 
 let controlFlow = {
     findTurn: function () {
@@ -39,56 +37,66 @@ let controlFlow = {
             event.target.textContent = currentPlayer.marker;
             controlFlow.switchTurn();
 
-            // Check for a winner
-            const winner = controlFlow.findWinner();
-            if (winner) {
-                alert(`${winner} is the winner!`);
-                // You can add more game-ending logic here
+            const winner = controlFlow.findWinner()
+            if (winner){
+            console.log(`Player with ${winner} marker won!`)
+            }else{
+              controlFlow.findDraw()
             }
         }
     },
-    findWinner: function () {
-        // Define the winning combinations (you may need more depending on your game rules)
-        const winningCombinations = [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-            [0, 4, 8], [2, 4, 6] // Diagonals
-        ];
+    findWinner: function (){
+      const winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6] // Diagonals
+      ]
+      for (const combination of winningCombinations){
+        const [a,b,c] = combination
+        const cellA = cellElements[a]
+        const cellB = cellElements[b]
+        const cellC = cellElements[c]
 
-        // Iterate through each winning combination
-        for (const combination of winningCombinations) {
-            const [a, b, c] = combination;
-            const cellA = cellElements[a];
-            const cellB = cellElements[b];
-            const cellC = cellElements[c];
-
-            // Check if all three cells in the combination have the same text content (either 'X' or 'O')
-            if (cellA.textContent && cellA.textContent === cellB.textContent && cellA.textContent === cellC.textContent) {
-                // You have a winner!
-                return cellA.textContent;
-            }
+        if (cellA.textContent === cellB.textContent && cellB.textContent === cellC.textContent){
+          return cellA.textContent
+        } 
+      }
+      
+    },
+    findDraw: function(){
+      const clickedArray = []
+      cellElements.forEach((cell) => {
+        if(cell.textContent != ""){
+          clickedArray.push(cell)
         }
+        if (clickedArray.length == 9){
+          return true
+        }
+      })
 
-        // If no winner is found, return null to indicate a tie or ongoing game
-        return null;
+    },
+    nameChanger: function (playerName1,playerName2){
+      
+    },
+
+    gameStarter: function (){
+
     }
-};
+    
+}
+
+
 
 cellElements.forEach((cell) => {
     cell.addEventListener('click', controlFlow.placeMarker);
 });
+// Clean up the interface to allow players to put in their names, 
+// include a button to start/restart the game and
+//  add a display element that congratulates the winning player!
 
 
 
 
 
 
-
-
-// should I write code that I want for :
-// I will check for text content of each cell ,
-//  declare player 1 winner if 3 cells have X as the text content and same 
-// for player2 if cells have 0 as the text content
-
-// should I write this outside(where i can like console log it and check) or in the control flow (weird question)
 
